b8b38f17c950afe146f870885014064bbfd9b962
HaDeSMonsta <leon.poellinger+git@gmail.com>
2023-12-22 23:28:51 +0100

    Add set_json_data function and refactor get_json_data
    Added a new function 'set_json_data' to update JSON data based on a specified key and value. Also, refactored the 'get_json_data' function to use constants instead of hardcoded values for better maintainability and clarity. Furthermore, made use of the std::io::Write library to facilitate file creation and writing.

--- src/get_json.rs
+++ src/get_json.rs
@@ -1,4 +1,9 @@
 use std::fs::File;
+use std::io::Write;
+
+const NAME_FIELD: &str = "simBrief_userName";
+const KEY_FIELD: &str = "api_token";
+const FILE_PATH: &str = "userdata.json";
 
 pub enum JsonKey {
     Name,
@@ -37,7 +42,7 @@ pub enum JsonKey {
 /// assert_eq!(json_data, "John Doe");
 /// ```
 pub fn get_json_data(key: JsonKey) -> String {
-    let file = match File::open("userdata.json") {
+    let file = match File::open(&FILE_PATH) {
         Ok(f) => f,
         Err(_e) => panic!("{_e}"),
     };
@@ -46,9 +51,59 @@ pub fn get_json_data(key: JsonKey) -> String {
         .expect("File should be proper JSON");
 
     let val = match key {
-        JsonKey::Name => json.get("simBrief_userName").unwrap().to_string(),
-        JsonKey::Key => json.get("api_token").unwrap().to_string(),
+        JsonKey::Name => json.get(NAME_FIELD).unwrap().to_string(),
+        JsonKey::Key => json.get(KEY_FIELD).unwrap().to_string(),
     };
 
     val[1..val.len() - 1].to_string()
+}
+
+/// Sets the JSON data based on a given key and value.
+///
+/// # Arguments
+///
+/// * `key` - The key indicating which field to update.
+/// * `val` - The new value to set for the specified field.
+///
+/// # Panics
+///
+/// This function will panic if it is unable to create or write to the file specified by `FILE_PATH`.
+///
+/// # Examples
+///
+/// ```
+/// use my_module::JsonKey;
+///
+/// let key = JsonKey::Name;
+/// let val = String::from("John Doe");
+///
+/// my_module::set_json_data(key, val);
+/// ```
+pub fn set_json_data(key: JsonKey, val: String) {
+    let other_val = match key {
+        JsonKey::Name => get_json_data(JsonKey::Key),
+        JsonKey::Key => get_json_data(JsonKey::Name),
+    };
+
+    let contend = match key {
+        JsonKey::Name => {
+            String::from(format!(
+                "{{\n\
+                \t\"{NAME_FIELD}\": \"{val}\",\n\
+                \t\"{KEY_FIELD}\": \"{other_val}\"\n\
+                }}"
+            ))
+        }
+        JsonKey::Key => {
+            String::from(format!(
+                "{{\n\
+                \t\"{NAME_FIELD}\": \"{other_val}\",\n\
+                \t\"{KEY_FIELD}\": \"{val}\"\n\
+                }}"
+            ))
+        }
+    };
+
+    let mut file = File::create(&FILE_PATH).expect("Unable to create file");
+    write!(file, "{contend}").expect("Unable to write to file");
 }
